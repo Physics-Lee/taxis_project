4244ae5cd56f2aaae405ea3cc3ff721c4ae65ec6
Physics-Lee <hyperdunk2019@mail.ustc.edu.cn>
2023-10-24 10:14:48 +0800

    move files

--- others/mcd_mat_to_intermediate_files/correct ideal direction for exp negligence of NC/correct_ortho_direction.m
+++
@@ -1,9 +0,0 @@
-function run_disp_corrected = correct_ortho_direction(run_disp)
-n = length(run_disp);
-run_disp_corrected = cell(n,1);
-for i = 1:n
-    run_disp_now = run_disp{i};
-    run_disp_now(2,:) = -run_disp_now(2,:); % y
-    run_disp_corrected{i} = run_disp_now;
-end
-end
