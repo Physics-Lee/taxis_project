a8f8651cabac2ba6c0952f9fe743e49ddf095d49
Physics-Lee <hyperdunk2019@mail.ustc.edu.cn>
2023-11-05 10:21:51 +0800

    del extract run theta

--- upstream/eset_mat_to_intermediate_files/extract_run_theta_vector.m
+++
@@ -1,7 +0,0 @@
-function run_theta_vector = extract_run_theta_vector(eset)
-run_theta = extract_run_theta(eset);
-run_theta_vector = [];
-for i = 1:length(run_theta)
-    run_theta_vector = [run_theta_vector run_theta{i}];
-end
-end
