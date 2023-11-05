3fa6fa0ae604cd5687f7153247c6091cde0cc05e
Physics-Lee <hyperdunk2019@mail.ustc.edu.cn>
2023-11-05 20:29:12 +0800

    Poisson process

--- downstream/reorientation_strategy/Poisson process/plot_the_distribution_of_run_time_and_reorientation_time.m
+++ downstream/reorientation_strategy/Poisson process/plot_the_distribution_of_run_time_and_reorientation_time.m
@@ -41,6 +41,14 @@ if path ~= 0
                 edges = [5 5:5:200 200];
                 x_lim_range = [5,200]; % s
                 y_lim_range = [0,0.20]; % probability
+
+                % for Colbert
+                if contains(full_path,'Colbert')
+                    edges = [5 5:10:85 85];
+                    x_lim_range = [5,85]; % s
+                    y_lim_range = [0,0.50]; % probability
+                end
+
             elseif contains(full_path,'reorientation')
                 title_str = 'distribution of reorientation time';
                 edges = [0 0:2.5:60 60];
@@ -68,7 +76,18 @@ if path ~= 0
             save_file_name = title_str;
             save_full_path = fullfile(save_folder_path,save_file_name);
             saveas(gcf,save_full_path,'png');
-            pause(1);
+
+            % semilogy
+            set_semilogy();
+            add_linear_fit(time_of_disp, edges);
+            save_file_name = strcat(title_str,'_semilogy');
+            save_full_path = fullfile(save_folder_path,save_file_name);
+            saveas(gcf,save_full_path,'png');
+
+            %
+            pause(0.5);
+
+            %
             close;
 
         end
